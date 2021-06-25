8a7ca26dfcb762ecfc4c036394f4310a3886128e
aman <amansaini1407@gmail.com>
2021-06-26 00:51:22 +0530

    CK EDI FIX

--- src/main/resources/static/admin/admin.js
+++ src/main/resources/static/admin/admin.js
@@ -20,3 +20,19 @@ function deleteCategoryOrChapter(cid,pattern)
 	  }
 	});
 }
+
+
+  function DemoCtrl() {
+
+				  this.foo = 'foo';
+									  
+		  		 CKEDITOR.editorConfig = function (config) {
+				config.extraPlugins = 'confighelper';
+				 };
+	 CKEDITOR.replace('editor1');
+
+	}
+	
+	angular
+  .module('gaigDemo', ['gaigUiBootstrap'])
+  .controller('DemoCtrl', DemoCtrl);
